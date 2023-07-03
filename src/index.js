d9c8c63e99a2d68133843e2c8d89cc72b0e44afa
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-07-04 04:38:33 +0800

    adding mongodb using mongoose

--- src/index.js
+++ src/index.js
@@ -1,13 +1,44 @@
 const express = require('express');
 const serverless = require('serverless-http');
+const mongoose = require('mongoose');
 
 const app = express();
 const router = express.Router();
 
-router.get("/", (req, res) => {
-  res.json({
-    hello: "hi!"
+// MongoDB connection URI and options
+const mongoURI = 'mongodb+srv://hris-eo:7b1kysZMkSpoOKDS@cluster0.ymkqxds.mongodb.net/hrsystem?retryWrites=true&w=majority'; // Replace with your MongoDB connection URI
+const dbName = 'hrsystem'; // Replace with your MongoDB database name
+const mongoOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+// Connect to MongoDB
+mongoose.connect(mongoURI, mongoOptions)
+  .then(() => {
+    console.log('Connected to MongoDB');
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
   });
+
+// Define the route that interacts with the MongoDB database
+router.get("/", async (req, res) => {
+  try {
+    // Define a Mongoose model
+    const ExampleModel = mongoose.model('Example', new mongoose.Schema({
+      name: String,
+      age: Number
+    }));
+
+    // Query the database using the Mongoose model
+    const result = await ExampleModel.find();
+
+    res.json(result);
+  } catch (error) {
+    console.error('Error querying MongoDB:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 app.use("/.netlify/functions/index", router);
