0e80d04631e0f8dd2b31d7801943038f97637f4f
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-07-06 22:04:20 +0800

    fixing the sign-in page

--- functions/mongodb-connection.js
+++ functions/mongodb-connection.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
-const MongoClient = require('mongodb').MongoClient;
-const uri = 'mongodb+srv://empireone:hXCieVuIw5DvCX7z@serverlessinstance0.i4tpgor.mongodb.net/?retryWrites=true&w=majority'; // Use the environment variable MONGODB_URI
-const dbName = 'hrsystem_serverless';
+const { MongoClient } = require('mongodb');
+
+const uri = process.env.MONGODB_URI;
+const dbName = process.env.MONGODB_DATABASE;
 
 let client = null;
 
@@ -19,20 +20,17 @@ async function disconnect() {
   }
 }
 
-async function findUsers() {
+async function getConnectionStatus() {
   if (!client) {
     throw new Error('MongoDB client is not connected');
   }
 
-  const db = client.db(dbName);
-  const collection = db.collection('users');
-  const users = await collection.find().toArray();
-
-  return users;
+  const isConnected = client.isConnected();
+  return { connected: isConnected };
 }
 
 module.exports = {
   connect,
   disconnect,
-  findUsers,
+  getConnectionStatus,
 };
