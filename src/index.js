cc5dfebcefecf1dcc715c044fef77448addafc65
John Rodney Bargayo <johnrodney.bargayo@gmail.com>
2023-08-19 20:04:29 +0800

    updated the CORS origin

--- src/index.js
+++ src/index.js
@@ -6,12 +6,9 @@ const cors = require("cors");
 const authRoutes = require("./routes/authRoutes");
 const users = require("./routes/users");
 const inventoryRoutes = require("./routes/inventoryRoutes");
-const emailRoutes = require("./routes/emailRoutes"); // Import the email routes
-const applicantRoutes = require("./routes/applicantRoutes"); // Import the applicant routes
-const uploadImageRoutes = require("./routes/uploadImage"); // Import the uploadImage routes
-
-
-const path = require("path");
+const emailRoutes = require("./routes/emailRoutes");
+const applicantRoutes = require("./routes/applicantRoutes");
+const uploadImageRoutes = require("./routes/uploadImage");
 
 require("dotenv").config();
 
@@ -35,39 +32,32 @@ mongoose
 
 // Middleware
 app.use(bodyParser.json());
-app.use(cors());
-app.options("*", cors());
 
+// Configure CORS
 const allowedOrigins = [
   "https://hrsystem-dev.empireonecontactcenter.com",
   // Add more allowed origins as needed
 ];
 
 app.use(cors({
-  origin: (origin, callback) => {
+  origin: function (origin, callback) {
     if (!origin || allowedOrigins.includes(origin)) {
       callback(null, true);
     } else {
       callback(new Error("Not allowed by CORS"));
     }
   },
-  methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
-  credentials: true,
-  preflightContinue: false,
-  optionsSuccessStatus: 204,
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
 }));
-
-// Serve static files from the 'uploads' directory
-//app.use('/uploads', express.static('uploads'));
-// Assuming 'express' is imported and your app is defined as 'app'
 
 // Routes
 app.use("/api/users", users);
 app.use("/api/login", authRoutes);
 app.use("/api/inventory", inventoryRoutes);
-app.use("/api/emails", emailRoutes); // Add the email routes
-app.use("/api/applicants", applicantRoutes); // Add the applicant routes
-app.use("/api/uploadImage", uploadImageRoutes); // Add the uploadImage routes
+app.use("/api/emails", emailRoutes);
+app.use("/api/applicants", applicantRoutes);
+app.use("/api/uploadImage", uploadImageRoutes);
 
 // Error handling middleware
 app.use((err, req, res, next) => {
